bd42b39bf19096bf6c6d5bb83994ddae37e519d4
vnaidu <vnaidu@contextlogic.com>
2020-03-16 17:45:14 -0700

    Add celery workers for superset

--- contrib/docker/docker-entrypoint.sh
+++ contrib/docker/docker-entrypoint.sh
@@ -20,16 +20,18 @@ set -ex
 if [ "$#" -ne 0 ]; then
     exec "$@"
 elif [ "$SUPERSET_ENV" = "development" ]; then
-    celery worker --app=superset.sql_lab:celery_app --pool=gevent -Ofair &
-    # needed by superset runserver
-    (cd superset/assets/ && npm ci)
-    (cd superset/assets/ && npm run dev) &
-    FLASK_ENV=development FLASK_APP=superset:app flask run -p 8088 --with-threads --reload --debugger --host=0.0.0.0
+    celery worker --app=superset.sql_lab:celery_app --pool=gevent -Ofair -c 4  &
+    gunicorn --bind  0.0.0.0:8088 \
+        --workers $((2 * $(getconf _NPROCESSORS_ONLN) + 1)) \
+        --timeout 300 \
+        --limit-request-line 0 \
+        --limit-request-field_size 0 \
+        superset:app
 elif [ "$SUPERSET_ENV" = "production" ]; then
-    celery worker --app=superset.sql_lab:celery_app --pool=gevent -Ofair &
+    celery worker --app=superset.sql_lab:celery_app --pool=gevent -Ofair -c 4 &
     exec gunicorn --bind  0.0.0.0:8088 \
         --workers $((2 * $(getconf _NPROCESSORS_ONLN) + 1)) \
-        --timeout 60 \
+        --timeout 300 \
         --limit-request-line 0 \
         --limit-request-field_size 0 \
         superset:app
